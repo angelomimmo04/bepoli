03add260fc27677c417164758452b75ec1686b47
santogol <riccardosantomauro58@gmail.com>
2025-06-15 17:08:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -590,7 +590,7 @@ const postSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "Utente" },
   desc: String,
   image: {
-    path: Buffer,
+    data: Buffer,
     contentType: String
   },
   createdAt: { type: Date, default: Date.now },
