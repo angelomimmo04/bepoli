c93f7c7a626ba0eba810c41c3cb0b359cc04bb42
angelomimmo04 <a.mimmo@studenti.poliba.it>
2025-07-01 14:59:41 +0200

    Update localizzazione.js

--- public/localizzazione.js
+++ public/localizzazione.js
@@ -327,7 +327,7 @@
 
                    
                     }
-                }
+                
             },
             function (error) {
                 outputCoords.textContent = "Errore nella geolocalizzazione: " + error.message;
