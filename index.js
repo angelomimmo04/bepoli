db459ff8a1c3007f062601dfb32c8d0e84cf3d35
angelomimmo04 <a.mimmo@studenti.poliba.it>
2025-07-01 15:27:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -603,6 +603,7 @@ const postSchema = new mongoose.Schema({
     {
       userId: { type: mongoose.Schema.Types.ObjectId, ref: "Utente" },
       text: String,
+      location: String,
       createdAt: { type: Date, default: Date.now }
     }
   ]
