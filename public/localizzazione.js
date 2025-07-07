5d40b8fb1dee7bac1205b3fd2f74207e07e95dc8
angelomimmo04 <a.mimmo@studenti.poliba.it>
2025-07-07 03:20:43 +0200

    Update localizzazione.js

--- public/localizzazione.js
+++ public/localizzazione.js
@@ -260,10 +260,10 @@ function startTracking() {
 
     watchId = navigator.geolocation.watchPosition(
         (position) => {
-            //const lat = position.coords.latitude;
-            //const lon = position.coords.longitude;
-            const lat = 41.108082
-            const lon = 16.879843
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            //const lat = 41.108082
+            //const lon = 16.879843
             
             const accuracy = position.coords.accuracy;
 
